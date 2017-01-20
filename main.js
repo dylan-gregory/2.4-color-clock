9cd61aec535cd79cb5ab162e9607b92acc3d2b37
dylan-gregory <jdylangregory@gmail.com>
2017-01-20 15:44:18 -0500

    Starting to look okay

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
     var nowHour = nowTime.getHours();
     var nowMinute = nowTime.getMinutes();
     var nowSecond = nowTime.getSeconds();
+    var progress = nowSecond/120 * 100;
 
 
   document.getElementById('hours').innerHTML = ("0" + nowHour).slice(-2);
@@ -15,10 +16,28 @@
   document.getElementById('seconds').innerHTML = ("0" + nowSecond).slice(-2);
 
 
-  window.setInterval(checkTime, 1000);
-}
+if (nowSecond < 30) {
+ document.querySelector('.bg').style.backgroundColor = '#61AFEF';
+ }else {
+   document.querySelector('.bg').style.backgroundColor = '#98C379';
+ }
 
+ // if (document.querySelector('.bg').style.backgroundColor = '#61AFEF' && 'hover') {
+ //
+ // }
+
+document.getElementById('status-bar').style.width = progress + "%";
+
+
+
+
+}
 checkTime();
+window.setInterval(checkTime, 1000);
+
+
+
+
 
 
 }());
