5f17b3a38c42d236535f560594fa2ab1d388ff4e
dylan-gregory <jdylangregory@gmail.com>
2017-01-23 10:02:54 -0500

    Working on colors

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 
 
 
+    var clockFace = document.querySelector('.current-time');
+    var isHovering = false;
+
   function checkTime(){
     var nowTime = new Date();
     var nowHour = nowTime.getHours();
     var nowMinute = nowTime.getMinutes();
     var nowSecond = nowTime.getSeconds();
     var progress = nowSecond/200 * 100;
+    var clockDisplay;
 
     document.getElementById('hours').innerHTML = ("0" + nowHour).slice(-2);
     document.getElementById('minutes').innerHTML = ("0" + nowMinute).slice(-2);
@@ -22,6 +26,10 @@
     //    document.querySelector('.bg').style.backgroundColor = '#98C3A1';
     //  }
 
+    // function colorDisplay(nowHour, nowMinute, nowSecond){
+    //   nowHour =
+    // }
+
     document.querySelector('.bg').style.backgroundColor = '#' + '0123' + nowSecond;
 
 
@@ -29,12 +37,29 @@
     document.getElementById('status-bar2').style.width = progress + "%";
 
 
+    if (isHovering) {
+    /*use colorCode(hours, minutes seconds from bottom) */
+      document.getElementById('hours').innerHTML = "01";
+      document.getElementById('minutes').innerHTML = '23';
+      document.getElementById('seconds').innerHTML = ("0" + nowSecond).slice(-2);
+
+    }else {
+      document.getElementById('hours').innerHTML = ("0" + nowHour).slice(-2);
+      document.getElementById('minutes').innerHTML = ("0" + nowMinute).slice(-2);
+      document.getElementById('seconds').innerHTML = ("0" + nowSecond).slice(-2);
+    }
+
+}
+
+function handleMouseOut(){
+  isHovering = false;
 }
 
 
 function colorDisplay(){
   var nowTime = new Date();
   var nowSecond = nowTime.getSeconds();
+  isHovering = true;
 
   document.getElementById('hours').innerHTML = "01";
   document.getElementById('minutes').innerHTML = '23';
@@ -43,35 +68,27 @@ function colorDisplay(){
 
 }
 
-if (document.querySelector(".current-time").addEventListener('mouseenter', colorDisplay)) {
-  document.getElementById('hours').innerHTML = "01";
-  document.getElementById('minutes').innerHTML = '23';
-  document.getElementById('seconds').innerHTML = ("0" + nowSecond).slice(-2);
-}
 
+clockFace.addEventListener('mouseover', colorDisplay);
+clockFace.addEventListener('mouseout', handleMouseOut);
 
-// while (document.querySelector(".current-time").addEventListener('mouseover', colorDisplay)) {
-//   clearInterval(checkTime);
-// }
 
 
 checkTime();
 window.setInterval(checkTime, 1000);
-document.querySelector(".current-time").addEventListener('mouseover', colorDisplay);
 
+
+
+/* how to get hex code from current time
+
+// function colorCode(nowHour, nowMinute, nowSecond){
+//   nowHour = nowHour.toString(16);
+//   nowMinute = nowMinute.toString(16);
+//   nowSecond = nowSecond.toString(16);
 //
-//
-// function colorDisplay(){
-//   if (document.querySelector('.bg').style.backgroundColor = '#3444C4') {
-//     document.getElementById('hours').innerHTML = "C4";
-//     document.getElementById('minutes').innerHTML = "44";
-//     document.getElementById('seconds').innerHTML = "34";
-//   }
-//   if (document.querySelector('.bg').style.backgroundColor = '#98C3A1') {
-//     document.getElementById('hours').innerHTML = "98";
-//     document.getElementById('minutes').innerHTML = "C3";
-//     document.getElementById('seconds').innerHTML = "A1";
-//   }
+//   return [nowHour, nowMinute, nowSecond];
+// }
+*/
 
 
 
